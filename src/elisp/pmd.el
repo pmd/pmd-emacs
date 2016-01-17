cfa76c453826bf9b66a5544fa1edb4a61b0a11b2
Andreas Dangel <adangel@users.sourceforge.net>
2016-01-17 12:20:25 +0100

    Update changelog

--- src/elisp/pmd.el
+++ src/elisp/pmd.el
@@ -64,6 +64,7 @@
 
 ;; ???? 0.9:
 ;; - [PR#5](https://github.com/pmd/pmd-emacs/pull/5): Make pmd.el non-executable
+;; - [PR#6](https://github.com/pmd/pmd-emacs/pull/6): Fix trivial typo
 
 ;; 2016-01-14 0.8:
 ;; - Fixed license link
